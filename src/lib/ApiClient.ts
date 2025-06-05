8e702e4ec30bf35f94454e7581a8aa827aee1c14
Khaled <khaled.romeh@archerirm.com>
2025-06-05 21:25:08 +0300

    Added api key header

--- src/lib/ApiClient.ts
+++ src/lib/ApiClient.ts
@@ -5,12 +5,14 @@ export default class ApiClient {
     const token = JSON.parse(getCookie("RW_Token") as string) || {};
     const access_token = token["access_token"] || "";
     const baseURL = import.meta.env.VITE_API_URL;
+    const apiKey = import.meta.env.VITE_API_KEY;
     console.log(import.meta.env.VITE_API_URL);
     return axios.create({
       baseURL: baseURL,
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${access_token}`,
+        "x-api-key": apiKey,
       },
     });
   }
